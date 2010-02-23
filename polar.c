b7b699e3e27c9fa4491cc7a74e74a56f42770d84
Aristeu Rozanski <aris@ruivo.org>
2010-02-22 23:53:51 -0500

    Adding licensing
    Signed-off-by: Aristeu Rozanski <aris@ruivo.org>

--- polar.c
+++ polar.c
@@ -1,3 +1,21 @@
+/*
+ * This file is part of axn500.
+ *
+ * axn500 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * axn500 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with axn500. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * (C) Copyright 2010 Aristeu S. Rozanski F. <aris@ruivo.org>
+ */
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
